41bdba0ae45bc11992c591e1ca3c7bade7a78057
frippeosd <fredrik_rosberg@hotmail.com>
2022-10-04 12:10:51 +0200

    setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,7 @@
-hejsan
+const express= require("express")
+const PORT='3001'
+const app=express()
+app.get("/",(req, res)=>{res.send("<p>Hejsan va kul</p>")})
+
+
+app.listen(PORT, ()=>{console.log( `Listen on port:${PORT}`)})
